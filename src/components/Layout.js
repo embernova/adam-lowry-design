5a97451d16b15ce30e69e106b9d4173723b05cab
Robert Pope <21979231+pope-12@users.noreply.github.com>
2020-12-10 18:29:19 -0700

    Update font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -44,7 +44,7 @@ const TemplateWrapper = ({ children }) => {
         />
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;0,900;1,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,700&display=swap" rel="stylesheet" />
       </Helmet>
       <HeaderTemplate />
       <div>{children}</div>
